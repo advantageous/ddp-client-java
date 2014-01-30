fe637b24ec247329919f655d2453b02d9b1148a7
Geoffrey Chandler <geoffc@gmail.com>
2014-01-29 21:57:15 -0800

    cleaned up jfx example to simplify controller

--- examples/java-fx/src/main/java/org/meteor/sample/JavaFXWebSocket.java
+++ examples/java-fx/src/main/java/org/meteor/sample/JavaFXWebSocket.java
@@ -1,12 +1,39 @@
 package org.meteor.sample;
 
 import javafx.application.Application;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.glassfish.tyrus.client.ClientManager;
+import org.meteor.ddp.*;
+import org.meteor.ddp.subscription.MapSubscriptionAdapter;
+import org.meteor.ddp.subscription.ObjectConverterJson;
+import org.meteor.ddp.subscription.SubscriptionCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JavaFXWebSocket extends Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JavaFXWebSocket.class);
+
+    final Map<String, Map<String, Object>> localData = new HashMap<>();
+
+    final MapSubscriptionAdapter subscriptionAdapter;
+
+    final WebSocketClient client;
+
+    {
+        client = new WebSocketClient(ClientManager.createClient(), new MessageConverterJson());
+        client.registerHandler(this);
+        subscriptionAdapter = new MapSubscriptionAdapter(client, new ObjectConverterJson(), localData);
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -20,5 +47,49 @@ public class JavaFXWebSocket extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+
+        MeteorService thread = new MeteorService();
+        thread.start();
+    }
+
+    @MessageHandler(ConnectedMessage.class)
+    public void handleConnected(ConnectedMessage message) throws IOException {
+        LOGGER.info("connected: " + message.getSession());
+        subscriptionAdapter.subscribe("tabs", null, new SubscriptionCallback() {
+            @Override
+            public void onReady(String subscriptionId) {
+                LOGGER.info("subscription ready: " + subscriptionId);
+            }
+
+            @Override
+            public void onFailure(String subscriptionId, DDPError error) {
+                LOGGER.error("subscription failed: " + error);
+            }
+        });
+    }
+
+    @MessageHandler(ErrorMessage.class)
+    public void handleError(ErrorMessage message) {
+        LOGGER.error("error: " + message.getReason());
+    }
+
+    class MeteorService extends Service {
+
+        @Override
+        protected Task createTask() {
+            return new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    LOGGER.info("connecting to client.");
+                    client.connect("ws://localhost:3000/websocket");
+                    return null;
+                }
+            };
+        }
     }
 }
--- examples/java-fx/src/main/java/org/meteor/sample/SampleController.java
+++ examples/java-fx/src/main/java/org/meteor/sample/SampleController.java
@@ -1,45 +1,18 @@
 package org.meteor.sample;
 
-import javafx.concurrent.Service;
-import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
-import org.glassfish.tyrus.client.ClientManager;
-import org.meteor.ddp.*;
+import org.meteor.ddp.MessageHandler;
 import org.meteor.ddp.subscription.AddedMessage;
-import org.meteor.ddp.subscription.MapSubscriptionAdapter;
-import org.meteor.ddp.subscription.ObjectConverterJson;
-import org.meteor.ddp.subscription.SubscriptionCallback;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 public class SampleController implements Initializable {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SampleController.class);
-
-    final Map<String, Map<String, Object>> localData = new HashMap<>();
-
-    final MapSubscriptionAdapter subscriptionAdapter;
-
-    final WebSocketClient client;
-
-    {
-        client = new WebSocketClient(ClientManager.createClient(), new MessageConverterJson());
-        client.registerHandler(this);
-
-        subscriptionAdapter = new MapSubscriptionAdapter(client, new ObjectConverterJson(), localData);
-    }
 
     @FXML
     private TextArea messages;
@@ -50,34 +23,8 @@ public class SampleController implements Initializable {
     @FXML
     private TableColumn<RowData, String> column;
 
-    @FXML
-    private void handleButtonAction(ActionEvent event) throws IOException {
-
-    }
-
-    @MessageHandler(ConnectedMessage.class)
-    public void handleConnected(ConnectedMessage message) throws IOException {
-        log("connected: " + message.getSession());
-        subscriptionAdapter.subscribe("tabs", null, new SubscriptionCallback() {
-            @Override
-            public void onReady(String subscriptionId) {
-                log("subscription ready");
-            }
-
-            @Override
-            public void onFailure(String subscriptionId, DDPError error) {
-                log("subscription failed");
-            }
-        });
-    }
-
-    private void log(String text){
+    private void log(String text) {
         messages.appendText(text + "\n");
-    }
-
-    @MessageHandler(ErrorMessage.class)
-    public void handleError(ErrorMessage message) {
-        log("error: " + message.getReason());
     }
 
     @MessageHandler(AddedMessage.class)
@@ -90,23 +37,6 @@ public class SampleController implements Initializable {
         table.setEditable(true);
         column.setResizable(true);
         column.setCellValueFactory(new PropertyValueFactory<RowData, String>("message"));
-
-        MeteorService thread = new MeteorService();
-        thread.start();
     }
 
-    class MeteorService extends Service {
-
-        @Override
-        protected Task createTask() {
-            return new Task<Void>() {
-                @Override
-                protected Void call() throws Exception {
-                    LOGGER.info("connecting to client.");
-                    client.connect("ws://localhost:3000/websocket");
-                    return null;
-                }
-            };
-        }
-    }
 }
